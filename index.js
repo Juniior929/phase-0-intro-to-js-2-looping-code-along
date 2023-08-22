0d7c8fd160604cc5177381b59e1836b746963a14
+ <+>
2023-08-22 14:58:45 -0400

    looping code along

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event) {
+  const thankYouMessages = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return thankYouMessages;
+}
+
+const event = "surprise";
+const thankYouMessages = writeCards(names, event);
+console.log(thankYouMessages);
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
